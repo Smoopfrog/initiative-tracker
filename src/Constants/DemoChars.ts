8f0c64610da39c61574cc2a627cec2d234ec8f1b
Jeff Stinson <jeffstinson@hotmail.com>
2025-08-22 16:02:55 -0700

    rename characters

--- src/Constants/DemoChars.ts
+++ src/Constants/DemoChars.ts
@@ -4,7 +4,7 @@ export const initialCharacters: ICharacter[] = [
 	{
 		id: "1",
 		initiative: 10,
-		name: "John Doe",
+		name: "Brenda is cute",
 		hp: 10,
 		ac: 10,
 		conditions: ["Poisoned", "Prone"],
@@ -12,7 +12,7 @@ export const initialCharacters: ICharacter[] = [
 	{
 		id: "2",
 		initiative: 20,
-		name: "Jane Doe",
+		name: "Brenda is hot",
 		hp: 10,
 		ac: 10,
 		conditions: ["Blinded"],
@@ -20,7 +20,7 @@ export const initialCharacters: ICharacter[] = [
 	{
 		id: "3",
 		initiative: 30,
-		name: "Alex Doe",
+		name: "Brenda is a bitch (This was ai generated)",
 		hp: 10,
 		ac: 10,
 		conditions: [],
@@ -28,7 +28,7 @@ export const initialCharacters: ICharacter[] = [
 	{
 		id: "4",
 		initiative: 40,
-		name: "Alexandra Doe",
+		name: "Brenda is smart",
 		hp: 10,
 		ac: 10,
 		conditions: ["Frightened", "Grappled"],
@@ -36,7 +36,7 @@ export const initialCharacters: ICharacter[] = [
 	{
 		id: "5",
 		initiative: 50,
-		name: "Bobobobo",
+		name: "Brenda is a nerd (also ai generated)",
 		hp: 10,
 		ac: 10,
 		conditions: [],
