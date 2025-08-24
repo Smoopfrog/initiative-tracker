469ccba6553bca0123e24734f8466ff1f1794c22
Brendan Brown <brendan94brown@gmail.com>
2025-08-24 12:27:17 -0700

    update import to add useEffect for local storage

--- src/InitiativeTracker/InitiativeTracker.tsx
+++ src/InitiativeTracker/InitiativeTracker.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import InitiativeTrackerTable from "./Table/InitiativeTrackerTable";
 import InitiativeTrackerMenu from "./InitiativeTrackerMenu";
 import { initialCharacters } from "../Constants/DemoChars";
