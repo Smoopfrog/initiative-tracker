dc7aa110713551063d038fc7440eff3f860452d7
Jeff Stinson <jeffstinson@hotmail.com>
2025-08-26 22:01:45 -0700

    add docstring

--- src/InitiativeTracker/HP/HPRenderer.tsx
+++ src/InitiativeTracker/HP/HPRenderer.tsx
@@ -3,6 +3,10 @@ import type { ICharacter } from "../../Types/Character";
 import type { IColumnProps } from "../../Components/Table/Column.tsx/Column";
 import DefaultColumn from "../../Components/Table/Column.tsx/DefaultColumn";
 
+/**
+ * HP renderer component
+ * Displays the HP and max HP of a character
+ */
 const HPRenderer: React.FC<IColumnProps<ICharacter>> = (props) => {
 	return (
 		<div style={{ display: "flex", alignItems: "center" }}>
